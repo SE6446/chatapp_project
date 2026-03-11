f12f0f4397655d931f244c91cf6c343ce193fc2f
Nathan <natatkinson05@gmail.com>
2026-03-11 13:28:11 +0000

    added chat member methods (get,add,kick), tweaked things

--- chatapp/src/main/java/com/chatapp/baseClasses/Chat.java
+++ chatapp/src/main/java/com/chatapp/baseClasses/Chat.java
@@ -9,32 +9,62 @@ public class Chat implements Serializable{
 
     private LinkedList<Message> chat;
     private ArrayList<Profile> members;
+    private Profile host;
+    private boolean groupChat;
     public Scanner s =  new Scanner(System.in);
 
     public Chat() {
         chat = new LinkedList<>();
         members = new ArrayList<>() ;
+        groupChat = false;
+        host = null;
     }
 
     public LinkedList<Message> getChat() {
         return chat;
     }
 
-    public LinkedList<Message> displayChat() {
-        // to display chat from most recent to earliest- can maybe do .toArray()
+    public void displayChat() {
+        for (Message m : chat) {
+            System.out.println(m.getText());
+        }
     }
 
-    public Message sendMessage(Message message){
+    public ArrayList<Profile> getMembers() {
+        return members;
+    }
+
+    public void addMember(Profile p) {
+        members.add(p);
+    }
+
+    public void kickMember(Profile kicker, Profile kicked) {
+        if (groupChat) {
+
+            if (kicker != host) {
+                System.out.println("Permission Denied: You are not the host of this chat.");
+                return;
+            } else {
+                members.remove(kicked);
+            }
+
+        } else {
+            System.out.println("You cannot kick a member from a private chat.");
+        }
+    }
+
+    public Message sendMessage(Message message){ // .addFirst so that the most recent message will always appear at screen bottom
         chat.addFirst(message);
         return message;
     }
 
     public boolean deleteMessage(Message message){
+        //will add function to ensure messages can't be deleted by users that aren't the sender.
         boolean confirmed = false;
 
         while (!confirmed){
             //using system.out just to get the logic there before using swing
-            System.out.println("Are you sure you want to delete this message?");
+            System.out.println("Are you sure you want to delete this message? :\n" + "'"+message.getText()+"'" );
             String result = s.nextLine();
 
             if (result.equalsIgnoreCase("yes")){
