5ab12b7e67ddff182a5c547f0fc597b2e9570372
Nathan <natatkinson05@gmail.com>
2026-03-11 12:53:28 +0000

    Updated deleteMessage method and some fields

--- chatapp/src/main/java/com/chatapp/baseClasses/Chat.java
+++ chatapp/src/main/java/com/chatapp/baseClasses/Chat.java
@@ -3,11 +3,13 @@ package com.chatapp.baseClasses;
 import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Chat implements Serializable{
 
     private LinkedList<Message> chat;
     private ArrayList<Profile> members;
+    public Scanner s =  new Scanner(System.in);
 
     public Chat() {
         chat = new LinkedList<>();
@@ -27,9 +29,23 @@ public class Chat implements Serializable{
         return message;
     }
 
-    public Message deleteMessage(Message message){
-        chat.remove(message);
-        return message;
+    public boolean deleteMessage(Message message){
+        boolean confirmed = false;
+
+        while (!confirmed){
+            //using system.out just to get the logic there before using swing
+            System.out.println("Are you sure you want to delete this message?");
+            String result = s.nextLine();
+
+            if (result.equalsIgnoreCase("yes")){
+                chat.remove(message);
+                confirmed = true;
+
+            }else if (result.equalsIgnoreCase("no")){
+                return false;
+            }
+        }
+        return true;
     }
 
     //Adding editing from Message class.
