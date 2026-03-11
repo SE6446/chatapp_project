fbc696022c841bec179b689d3abe8576226c4a98
Nathan <natatkinson05@gmail.com>
2026-03-11 12:17:08 +0000

    Added simple methods to add and remove from the chat, and some necessary fields

--- chatapp/src/main/java/com/chatapp/baseClasses/Chat.java
+++ chatapp/src/main/java/com/chatapp/baseClasses/Chat.java
@@ -1,7 +1,37 @@
 package com.chatapp.baseClasses;
 
 import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class Chat implements Serializable{
-    // Implement LinkedList
+
+    private LinkedList<Message> chat;
+    private ArrayList<Profile> members;
+    private Message front;
+
+    public Chat() {
+        chat = new LinkedList<>();
+        front = chat.getFirst() ;
+    }
+
+    public LinkedList<Message> getChat() {
+        return chat;
+    }
+
+    public LinkedList<Message> displayChat() {
+
+    }
+
+    public Message sendMessage(Message message){
+        chat.addFirst(message);
+        return message;
+    }
+
+    public Message deleteMessage(Message message){
+        chat.remove(message);
+        return message;
+    }
+
+
 }
