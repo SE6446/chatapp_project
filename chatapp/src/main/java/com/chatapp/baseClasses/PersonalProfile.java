ac833355c03158924e2e15ca8172fe1eee084600
Adam M <Thirtynine363@gmail.com>
2026-03-18 11:54:59 +0000

    Added a contact list using Array Lists and then a function to add contacts to that list in PersonalProfile.java

--- chatapp/src/main/java/com/chatapp/baseClasses/PersonalProfile.java
+++ chatapp/src/main/java/com/chatapp/baseClasses/PersonalProfile.java
@@ -2,6 +2,8 @@ package com.chatapp.baseClasses;
 
 import java.io.Serializable;
 import java.util.UUID;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PersonalProfile implements Profile, Serializable {
     private int phoneNumberID;
@@ -10,7 +12,7 @@ public class PersonalProfile implements Profile, Serializable {
 
     private UUID uuid;
 
-    
+    private ArrayList<Contact> contactsList;
 
     @Override
     public int getphoneNumber() {
@@ -26,14 +28,33 @@ public class PersonalProfile implements Profile, Serializable {
         name = handle;
     }
 
+    @Override
+    public UUID getUUID() {
+        return uuid;
+    }
+
     public PersonalProfile(String name, int number){
         phoneNumberID = number;
         this.name = name;
         uuid = UUID.randomUUID();
     }
 
-    @Override
-    public UUID getUUID() {
-        return uuid;
+    public void addContact(Contact contact){
+        contactsList.add(contact);
     }
+
+    public void addContact(Scanner s){ // Manual Version
+        // creating a new contact
+        Contact contact = new Contact();
+
+        // set values
+        System.out.println("Please enter the name of the contact:\n");
+        contact.setHandle(s.nextLine());
+        System.out.println("Please enter the phone number of the contact:\n");
+        contact.setphoneNumber(s.nextInt());
+
+        // add to the contact list
+        contactsList.add(contact);
+    }
+
 }
