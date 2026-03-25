e2656c413d72b4662afa8581be02190fb810f6a8
Harry <Harybro11@gmail.com>
2026-03-25 12:18:48 +0000

    Started the GUI

--- chatapp/src/main/java/com/chatapp/UI/GUI.java
+++ chatapp/src/main/java/com/chatapp/UI/GUI.java
@@ -3,10 +3,30 @@ package com.chatapp.UI;
 import com.chatapp.App;
 import com.chatapp.baseClasses.*;
 import javax.swing.*;
+import java.awt.*;
 
 
+public class GUI extends JFrame {
 
-public class GUI {
+    private App app;
+    
+    private JTextField nameField;
+    private JTextField NumberField;
+    private JButton createButton;
+
+    public GUI(App app) {
+        this.app = app;
+
+        setTitle("Chat App");
+        setSize(350, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        initComponents();
+        setVisible(true);
+    }
+
+    
     
     
 }
