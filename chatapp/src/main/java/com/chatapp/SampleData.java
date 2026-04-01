e74e435694f14a5ca157b7f8378c0e3b46577fb8
Archie Macdonald <archiemac07@outlook.com>
2026-04-01 14:04:14 +0100

    Updated SampleData and added the sample data in question

--- chatapp/src/main/java/com/chatapp/SampleData.java
+++ chatapp/src/main/java/com/chatapp/SampleData.java
@@ -25,9 +25,23 @@ public class SampleData {
         contactIds[contactIds.length - 1] = UUID.randomUUID();
         for (int i = 0; i < contactIds.length -1; i++) {
             Chat chat = new Chat(sampleContacts.get(contactIds[i]));
+            switch (i) {
+                case 0:
+                    chat.sendMessage(new Message("I NEED vicodin [insert lightly racist/misogynistic quip]", sampleContacts.get(contactIds[i])));
+                    break;
+                case 1:
+                    chat.sendMessage(new Message("600 hours clinic duty", sampleContacts.get(contactIds[i])));
+                    break;
+                case 2:
+                    chat.sendMessage(new Message("I too am in this episode", sampleContacts.get(contactIds[i])));
+            
+                default:
+                    break;
+            }
             sampleChats.put(contactIds[i], chat);
         }
         Chat groupChat = new Chat(sampleContacts.values());
+        Message[] messages = {new Message("I NEED Vicoding Cuddy",sampleContacts.get(contactIds[1])), new Message("No, 6 billion years clinic duty",sampleContacts.get(contactIds[2])), new Message("I too am in this conversation",sampleContacts.get(contactIds[0]))};
         sampleChats.put(contactIds[contactIds.length - 1], groupChat);
     }
 
@@ -43,5 +57,6 @@ public class SampleData {
             sampleContacts.put(ids[i], contact);
         }
     }
+
 
 }
